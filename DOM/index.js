1f311a4d70c907275b18d53603c227eeb0625c2c
Amanderson Rodrigo <171895801+amandersonrodrigo@users.noreply.github.com>
2025-10-23 03:07:34 -0300

    a

--- DOM/index.js
+++ DOM/index.js
@@ -1,3 +1,11 @@
 document.querySelector("input#check").click();
 
 document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Amanderson"
+
+document.querySelector(".botao").style.color = "red"
+
+document.querySelector("ul li a").style.color = "green"
+
+document.querySelector(".botao").style.backgroundColor = "yellow"
+
+document.querySelector("#titulo").classList.add("huge")
